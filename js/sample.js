084d864a345289020a9ac70fdb57b62f06624e1a
arnoldparge <pargearnold@ymail.com>
2017-05-16 15:23:40 +0530

    Add files via upload

---
+++ js/sample.js
@@ -0,0 +1,11 @@
+﻿function openAdd() {
+    window.open("add.html", "_self");
+}
+
+function openSearch() {
+    window.open("search.html", "_self");
+}
+
+function toHome() {
+    window.open("index.html", "_self");
+}
